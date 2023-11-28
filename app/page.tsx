9d41c56e76cf8337304561bd2643ab38a4e84b42
irishkasite <152328129+irishkasite@users.noreply.github.com>
2023-11-28 23:12:08 +0200

    52

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="heart">❤</div>
       <script type="text/javascript" src="./script.js" async />
+
+
       <link
   rel="shortcut icon"
-  href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='.8 .8 14.4 14.4'><path d='m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z' fill='%2380b0ff' stroke='%235D7FDDaa' stroke-width='.3'/></svg>"
+  href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z' fill='%2380b0ff' stroke='%235D7FDDaa' stroke-width='.3'/></svg>"
 ></link>
     </main>
   )
