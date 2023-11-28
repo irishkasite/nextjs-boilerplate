d53bb435a0bf944b48d2edbfba53859da3d9a285
irishkasite <152328129+irishkasite@users.noreply.github.com>
2023-11-28 23:15:50 +0200

    w34d

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,8 @@ export default function Home() {
       <script type="text/javascript" src="./script.js" async />
 
 
-      <link
-  rel="shortcut icon"
-  href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z' fill='%2380b0ff' stroke='%235D7FDDaa' stroke-width='.3'/></svg>"
-></link>
+      <link rel="icon" type="image/x-icon" href="https://www.svgrepo.com/show/362109/heart.svg" />
+
     </main>
   )
 }
